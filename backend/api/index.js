94219a6ef36e6471f946969f8eeef93a7ab682fb
imadzl <154994303+imadzl@users.noreply.github.com>
2024-07-22 14:19:41 +0300

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -34,7 +34,7 @@ app.use('/images',express.static('upload/images'))
 app.post("/upload",upload.single('product'),(req,res)=>{
     res.json({
         success:1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
+        image_url:`https://books-store-backend-lovat.vercel.app:${port}/images/${req.file.filename}`
     })
 })
 
